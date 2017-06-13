f1e927ae4e424140bbcabbdacc707a3728b295eb
Jeremy Coyle <jeremyrcoyle@gmail.com>
2017-06-13 11:28:04 -0700

    cv error handling

--- tests/testthat/test_cross_validate.R
+++ tests/testthat/test_cross_validate.R
@@ -2,3 +2,22 @@ library(origami)
 library(data.table)
 context("Main Cross-Validation Method")
 
+############################
+# test error handling for failed folds
+
+cv_errors=function(fold){
+	index=fold_index()
+	if(index==7){
+		stop("7 makes me sad")
+	} 
+	
+	
+	return(list(a="certainly not 7"))
+}
+
+
+folds=make_folds(1000)
+
+results=cross_validate(cv_errors,folds,.foreach_control = list(.errorhandling="pass"))
+
+test_that("Errors are put in special error vector",expect_length(results$errors$error,1))
