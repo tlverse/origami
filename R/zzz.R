97bb5a8fd70049f5227b8e440412ed2b4ef75b12
Nima Hejazi <nh@nimahejazi.org>
2017-07-17 08:25:19 -0700

    minor vignette edits, add startup message

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+.onAttach <- function(...) {
+  packageStartupMessage("origami: Generalized Cross-Validation Framework")
+  packageStartupMessage("Version: ",
+                        utils::packageDescription("origami")$Version)
+}
